d95089f304ed14fab9dac0618b73733858fa54e3
Kathy Galang <kigalang@outlook.com>
2019-09-16 01:12:20 +0300

    calc

--- CSharp/helloworld/Program.cs
+++ CSharp/helloworld/Program.cs
@@ -2,11 +2,42 @@
 
 namespace helloworld
 {
+
+    /*
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            string name;
+            \\Console.WriteLine("What is your name?");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello there,"+ name);
+            
+
+                     
+
+
+        }
+    }
+    */
+
+    class Demo 
+    {
+        static void Main() 
+        {
+            
+            int sum;
+
+            Console.WriteLine ("Let's do some addition:");
+            Console.Write("Enter a number: ");
+            int num1= Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            int num2= Convert.ToInt32(Console.ReadLine());
+            sum = num1 + num2;        
+            //Console.WriteLine("The result is: {0}", sum);
+            Console.WriteLine("{0} + {1} = {2}", num1, num2, sum);
+
         }
     }
 }
