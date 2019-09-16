8bbc1b4a60256fde33e4d871ea8843e64e1d8255
Kathy Galang <kigalang@outlook.com>
2019-09-16 10:52:45 +0300

    update

--- Javacript/variables.js
+++ Javacript/variables.js
@@ -1,5 +1,18 @@
-var temp = 13;
-var age;
-var address;
+var temp = -20;
+
+console.log("Starting exercise.");
+
+
+if (temp> 25)
+{
+    console.log("It's a warm day");
+}
+
+else if  {temp < 1) {console.log("It's freezing today!");}
+
+else {
+    console.log("It's a bit cold today");
+    }
+}
 
 console.log(temp);
