e3af67c094ce646cefd9bf8a1bab27c054c7061c
Kathy Galang <kigalang@outlook.com>
2019-09-16 09:59:09 +0300

    new files

---
+++ CSharp/greetings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace greetings
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string name;
+            Console.WriteLine("What is your name?");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello there,"+ name);
+            
+                     
+        }
+    }
+    
+}
