633bacf2be1955097e4246cbe4e909cf547054a8
Kathy Galang <kigalang@outlook.com>
2019-10-08 15:07:36 +0300

    add more functions

--- Javacript/react-front/src/OwnComponent.js
+++ Javacript/react-front/src/OwnComponent.js
@@ -1,22 +1,54 @@
 import React from 'react';
 
 class OwnComponent extends React.Component {
+
+    constructor(props) {
+        console.log("OwnComponent.constructor()");
+        
+        super(props);
+        this.state = { asiakkaat:[] };
+    }
+
+    componentDidMount() {
+        console.log("OwnComponent.componentDidMount()");
+
+        let reactKomponentti = this;
+        fetch('https://localhost:5001/api/customers')
+            .then(response => response.json())
+            .then(json => {
+                console.log("Asiakas-data ladattu.");
+
+                reactKomponentti.setState({ asiakkaat: json });
+            });
+
+        console.log("Fetch-kutsu tehty");
+    }
+
     render() {
-        var luvut= [];
-        for (let index=1; index<=10; index++)
-        {
-            luvut.push(<p>{index}</p>); 
+        console.log("OwnComponent.render()");
+
+        var taulukko = [];
+        for (let index = 1; index <= 10; index++) {
+            taulukko.push(<tr>
+                <td>{index}-1</td>
+                <td>{index}-2</td>
+                <td>{index}-3</td>
+            </tr>);
         }
 
-        var lisäKoodia = <p>Tässä on tavallista tekstiä.</p>;
-
         return <div>
-            <h1>Tämä on oma komponenttini!</h1>
-            <p>Testi</p>
-            {lisäKoodia}
-            {luvut}
-
-            <h1>React Dynamic Table</h1>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Sarake 1</th>
+                        <th>Sarake 2</th>
+                        <th>Sarake 3</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {taulukko}
+                </tbody>
+            </table>
         </div>;
     }
 }
