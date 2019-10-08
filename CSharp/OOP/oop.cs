81eb0cb693dbc56c3c3b1a562879437ce9c469ac
Kathy Galang <kigalang@outlook.com>
2019-10-08 22:28:36 +0300

    add more functionality - in progress

--- CSharp/OOP/oop.cs
+++ CSharp/OOP/oop.cs
@@ -6,8 +6,44 @@ namespace OOP
     {
         static void Main(string[] args)
         {
-    
+            //int[] CarMaxSpeed;
+            //CarMaxSpeed = new int[5];
             
+            const int max = 200;
+            const int min = 80;
+            Random random = new Random();
+           
+            //fill the array with random speeds
+            for (int ctr = 0; ctr < 5; ctr++)
+            {
+                //CarMaxSpeed[ctr] = random.Next(min, max);
+                //System.Console.WriteLine("{0}", CarMaxSpeed[ctr]);
+                Car a = new Car();
+                a.Color = "red";
+                a.MaxSpeed = random.Next(min, max);
+                System.Console.WriteLine("speed: " + a.MaxSpeed); 
+                a.Start();
+                //Console.WriteLine("Car is running:" + a.MotorRunning);
+                if (a.Speed <= a.MaxSpeed) 
+                {
+                    a.Accelerate();
+                }
+                else 
+                {
+                    System.Console.WriteLine("Car is overspeeding, do you want to press the brakes (y/n)?");
+                    string answer = Console.ReadLine();
+                    string pos = "y";
+                    if (String.Equals(pos, answer))    
+
+
+                }
+                Console.WriteLine("Car has accelerated. The speed is now: "+ a.Speed);
+                
+             
+            }
+
+           
+/* 
             Car a = new Car();
             a.Color = "red";
             a.MaxSpeed = 200;
@@ -30,17 +66,14 @@ namespace OOP
             b.Accelerate();
             Console.WriteLine("Car B's speed: " + b.Speed);
 
-
-    
-
             BigTruck TruckA = new BigTruck();
             TruckA.MaxSpeed = 80;
             TruckA.Color = "white";
             TruckA.Capacity = 40;
-            TruckA.Speed = 0;
+            //TruckA.Speed = 0;
             TruckA.Start();
             PrintColor(TruckA); 
-        
+    */
         }
 
         static void PrintColor (Car varA)
