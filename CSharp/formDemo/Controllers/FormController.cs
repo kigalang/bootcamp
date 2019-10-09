c0e1b9625feeee814d318b3bc795b5cbe8282a14
Kathy Galang <kigalang@outlook.com>
2019-10-09 15:00:56 +0300

    add simple form

--- CSharp/formDemo/Controllers/FormController.cs
+++ CSharp/formDemo/Controllers/FormController.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
 
 namespace formDemo.Controllers
 {
@@ -35,8 +35,3 @@ namespace formDemo.Controllers
     }
 }
 
-Pricing
-API
-Training
-Blog
-About
