732300217d290c46a2c87b6d9ba7517a13cb4174
Kathy Galang <kigalang@outlook.com>
2019-10-14 11:27:01 +0300

    add new buttons

--- Javacript/react-front/src/App.js
+++ Javacript/react-front/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 import OwnComponent from './OwnComponent';
-
+import Nappi from './Nappi';
 function App() {
   return (
     <div className="App">
-      <OwnComponent />
+    
+      <Nappi teksti="Oma Nappi" />
+      <Nappi teksti="Lataa tiedot" />
+      <Nappi teksti="Tallenna" />
+      <Nappi teksti="Peruuta" />
+      <OwnComponent />   
     </div>
   );
 }
