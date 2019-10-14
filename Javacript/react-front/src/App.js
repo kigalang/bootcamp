eab6bcc71f1d98f659f825f4a47e6c8e272d607a
Kathy Galang <kigalang@outlook.com>
2019-10-14 15:11:48 +0300

    finish calc component

--- Javacript/react-front/src/App.js
+++ Javacript/react-front/src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './App.css';
 import Calc from './Calc'
+
 function App() {
   return (
-    <div className="App">
-    
+    <div className="App"> 
     <Calc />
-
-  
-
     </div>
   );
 }
 
 export default App;
+
+
+{/*test*/}
--- Javacript/react-front/src/Calc.js
+++ Javacript/react-front/src/Calc.js
@@ -2,15 +2,41 @@ import React from 'react';
 
 class Calc extends React.Component {
 
+
+    constructor(props){
+
+    super (props)
+    console.log ("constructor test");
+    this.var1Change = this.var1Change.bind(this);
+    this.var2Change = this.var2Change.bind(this);
+    this.buttonClick = this.buttonClick.bind(this);
+
+    }
+
     buttonClick(){
       console.log ("button clicked");
+      let sum = parseInt(this.state.var1) + parseInt(this.state.var2);
+        alert("Sum is: " + sum);
     }
+
+    var1Change (event){
+    console.log ("var1 method");
+    this.setState ({var1: event.target.value});
+    } 
+
+    var2Change (event){
+    console.log ("var2 method");
+    this.setState ({var2: event.target.value});
+
+    } 
+
+
 
     render() {
       return <div>
             <h1>Form</h1>
-            <p> <input type="text" /> </p>
-            <p> <input type="text" /> </p>
+            <p> <input type="text" onChange={this.var1Change}/> </p>
+            <p> <input type="text" onChange={this.var2Change}/> </p>
             <p> <button onClick={this.buttonClick}>Calculate</button> </p>
         </div>;
 
